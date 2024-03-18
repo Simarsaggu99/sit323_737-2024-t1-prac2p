e65f0306c0319629a902922e8addeac47a74b4f2
simarsaggu <simarsaggu1999@gmail.com>
2024-03-18 13:34:30 +1100

    reverse string function

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 
 const port =3000;
+
+const reverseString =(str) => {
+const reversedString = str.split("").reduce((acc, char) => char + acc, ""); 
+return String(reversedString)
+}
+
+app.get("/reverseString", (req, res)=>{
+    const stringToBeReversed = String(req.query.string);
+    const result = reverseString(stringToBeReversed);
+    res.json({
+        data : result,
+        statuscode: 200
+    }
+    )
+})
 app.listen(port, () =>{
     console.log("listening to port:" + port)
 })
